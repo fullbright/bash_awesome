7fa35344cfebdff77a389218c0dbbe3d6291ba92
Full Bright <full3right@gmail.com>
2016-04-12 23:01:23 +0200

    Test if script has been already added

--- install_awesome_bash.sh
+++ install_awesome_bash.sh
@@ -5,10 +5,12 @@ cp ./bashrcs/bash_awesome ~/.bash_awesome
 
 echo "If not there yet, add the code to read the bash_awesome script"
 
+if grep -q "bash_awesome" ~/.bashrctest; then
 cat >~/.bashrctest <<EOL
 if [ -f ~/.bash_awesome ]; then
     . ~/.bash_awesome
 fi
 EOL
+fi
 
 echo "Installed the Awesome bash configuration successfully! Enjoy :-)"
