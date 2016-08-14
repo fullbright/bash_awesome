8bdc6d82bc5a091d0845e9f1f456d9e31f96bd2e
Full Bright <full3right@gmail.com>
2016-08-14 15:34:14 +0200

    Fixing the append code to .bashrc

--- install_awesome_bash.sh
+++ install_awesome_bash.sh
@@ -2,12 +2,21 @@ cd ~/.bash_awesome
 
 echo "If not there yet, add the code to read the bash_awesome script"
 
-if grep -q "bash_awesome" ~/.bashrc; then
-cat >~/.bashrc <<EOL
+if grep -q bash_awesome ~/.bashrc; then
+    echo "bash_awesome is already added to bashrc. Skipping ..."
+else
+    echo "bash awesome is not installed yet on .bashrc"
+    echo "Adding ..."
+
+cat >>~/.bashrc <<EOL
+#
+# Bash_awesome add some extra commands to your bash console
 if [ -f ~/.bash_awesome/bashrcs/bash_awesome ]; then
     . ~/.bash_awesome/bashrcs/bash_awesome
 fi
 EOL
+
+    echo "Installed the Awesome bash configuration successfully! Enjoy :-)"
 fi
 
-echo "Installed the Awesome bash configuration successfully! Enjoy :-)"
+echo "Done."
